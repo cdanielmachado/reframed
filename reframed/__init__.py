03ce789cf461cc35e41794a30773a2a88c66d85e
Daniel <cdanielmachado@gmail.com>
2019-10-09 15:32:22 +0200

    update to 0.1.1

--- reframed/__init__.py
+++ reframed/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = "Daniel Machado"
 __email__ = 'cdanielmachado@gmail.com'
-__version__ = '__version__ = '__version__ = '0.1.0'''
+__version__ = '0.1.1'
 
 
 from .core.model import Model, Metabolite, Compartment, Reaction, ReactionType
