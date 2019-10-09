9c2d3327858515bf619bd672acc31f905a27fd49
Daniel <cdanielmachado@gmail.com>
2019-10-09 16:03:30 +0200

    first major release

--- setup.py
+++ setup.py
@@ -34,6 +34,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cdanielmachado/reframed',
-    version='0.1.1',
+    version='1.0.0',
     zip_safe=False,
 )
