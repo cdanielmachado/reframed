765f2cd2604be973d10cf4ef4cfed13e3532d881
Daniel <cdanielmachado@gmail.com>
2020-03-31 11:46:49 +0200

    community solution refactoring

--- reframed/community/solution.py
+++ reframed/community/solution.py
@@ -1,4 +1,6 @@
 from ..solvers.solution import print_values, print_balance
+from ..core.elements import molecular_weight
+import pandas as pd
 
 
 class CommunitySolution(object):
@@ -61,20 +63,6 @@ class CommunitySolution(object):
             self.internal[org_id] = fluxes
             self.normalized[org_id] = rates
 
-    def print_external_fluxes(self, pattern=None, sort=False, abstol=1e-9):
-        print_values(self.exchange, pattern=pattern, sort=sort, abstol=abstol)
-
-    def print_internal_fluxes(self, org_id, normalized=False, pattern=None, sort=False, abstol=1e-9):
-        if normalized:
-            print_values(self.normalized[org_id], pattern=pattern, sort=sort, abstol=abstol)
-        else:
-            print_values(self.internal[org_id], pattern=pattern, sort=sort, abstol=abstol)
-
-    def print_external_balance(self, m_id, sort=False, percentage=False, equations=False, abstol=1e-9):
-
-        print_balance(self.solution.values, m_id, self.community.merged_model, sort=sort, percentage=percentage, equations=equations,
-                      abstol=abstol)
-
     # calculate overall exchanges (organism x metabolite) -> rate
 
     def compute_exchanges(self):
@@ -104,17 +92,66 @@ class CommunitySolution(object):
 
         return exchanges
 
-    def cross_feeding(self, abstol=1e-6):
+    def cross_feeding(self, as_df=False, abstol=1e-6):
         exchanges = self.compute_exchanges()
         cross_all = []
 
         for m_id in self.community.merged_model.get_external_metabolites():
             r_out = {x: r for (x, m), r in exchanges.items() if m == m_id and r > abstol}
             r_in = {x: -r for (x, m), r in exchanges.items() if m == m_id and -r > abstol}
-            total = max(sum(r_out.values()), sum(r_in.values()))
+
+            total_in = sum(r_in.values())
+            total_out = sum(r_out.values())
+            total = max(total_in, total_out)
+
+            if total_in > total_out:
+                r_out[None] = total_in - total_out
+            if total_out > total_in:
+                r_in[None] = total_out - total_in
+
             cross = [(o1, o2, m_id, r1 * r2 / total) for o1, r1 in r_out.items() for o2, r2 in r_in.items()]
             cross_all.extend(cross)
+
+        if as_df:
+            cross_all = pd.DataFrame(cross_all, columns=["donor", "receiver", "compound", "rate"])
+
         return cross_all
+
+    def mass_flow(self, as_df=False, abstol=1e-6):
+
+        def get_mass(x):
+            met = self.community.merged_model.metabolites[x]
+            formula = met.metadata.get('FORMULA', '')
+            mw = molecular_weight(formula)
+            return 0.001 * mw
+
+        entities = list(self.community.organisms) + [None]
+        flow = {(o1, o2): 0 for o1 in entities for o2 in entities}
+
+        for o1, o2, m_id, rate in self.cross_feeding():
+            flow[(o1, o2)] += get_mass(m_id) * rate
+
+        flow = {key: val for key, val in flow.items() if val > abstol}
+
+        if as_df:
+            flow = [(o1, o2, val) for (o1, o2), val in flow.items()]
+            flow = pd.DataFrame(flow, columns=["donor", "receiver", "flow"])
+
+        return flow
+
+    def print_external_fluxes(self, pattern=None, sort=False, abstol=1e-9):
+        print_values(self.exchange, pattern=pattern, sort=sort, abstol=abstol)
+
+    def print_internal_fluxes(self, org_id, normalized=False, pattern=None, sort=False, abstol=1e-9):
+        if normalized:
+            print_values(self.normalized[org_id], pattern=pattern, sort=sort, abstol=abstol)
+        else:
+            print_values(self.internal[org_id], pattern=pattern, sort=sort, abstol=abstol)
+
+    def print_external_balance(self, m_id, sort=False, percentage=False, equations=False, abstol=1e-9):
+
+        print_balance(self.solution.values, m_id, self.community.merged_model, sort=sort, percentage=percentage, equations=equations,
+                      abstol=abstol)
 
     def print_exchanges(self, m_id=None, abstol=1e-9):
 
