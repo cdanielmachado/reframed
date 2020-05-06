febcc7f54d3b02edc306e9374fb79c05d63acd2d
Daniel <cdanielmachado@gmail.com>
2020-05-06 17:16:48 +0200

    rm tmp.py

--- tests/tmp.py
+++
@@ -1,8 +0,0 @@
-from reframed import from_cobrapy, save_cbmodel
-from cobra.io import load_json_model
-
-cwd = '/Volumes/Data/Dropbox/embl/foodtranscriptomics/models/pangenomic/'
-
-cbmodel = load_json_model(cwd + "curated.json")
-model = from_cobrapy(cbmodel)
-save_cbmodel(model, cwd+"panmodel.xml", flavor="bigg")
