588a76498f7ec55dcb1214f29ecbd8491ea6fa69
Daniel Machado <cdanielmachado@gmail.com>
2024-07-04 10:56:09 +0200

    refactor solver

--- reframed/solvers/solver.py
+++ reframed/solvers/solver.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from math import inf
+from .solution import Solution, Status
 
 
 class VarType(Enum):
@@ -35,11 +36,13 @@ class Solver(object):
 
     def __init__(self, model=None):
         self.problem = None
-        self.var_ids = []
-        self.constr_ids = []
         self.model = model
+        self.variables = {}
+        self.constraints = {}
+        self._cached_vars = {}
+        self._cached_constrs = {}
 
-    def add_variable(self, var_id, lb=-inf, ub=inf, vartype=VarType.CONTINUOUS, update=True):
+    def add_variable(self, var_id, lb=-inf, ub=inf, vartype=VarType.CONTINUOUS):
         """ Add a variable to the current problem.
 
         Arguments:
@@ -47,10 +50,15 @@ class Solver(object):
             lb (float): lower bound
             ub (float): upper bound
             vartype (VarType): variable type (default: CONTINUOUS)
-            update (bool): update problem immediately (default: True)
         """
 
-    def add_constraint(self, constr_id, lhs, sense='=', rhs=0, update=True):
+        self._cached_vars[var_id] = (lb, ub, vartype)
+
+    def add_variables(self, var_dict):
+        """ Solver specific implementation """
+        pass
+
+    def add_constraint(self, constr_id, lhs, sense='=', rhs=0):
         """ Add a constraint to the current problem.
 
         Arguments:
@@ -58,83 +66,25 @@ class Solver(object):
             lhs (dict): variables and respective coefficients
             sense (str): constraint sense (any of: '<', '=', '>'; default '=')
             rhs (float): right-hand side of equation (default: 0)
-            update (bool): update problem immediately (default: True)
         """
+        
+        self._cached_constrs[constr_id] = (lhs, sense, rhs)
+
+    def add_constraints(self, constr_dict):
+        """ Solver specific implementation """
         pass
-
-    def remove_variable(self, var_id):
-        """ Remove a variable from the current problem.
-
-        Arguments:
-            var_id (str): variable identifier
-        """
-        pass
-
-    def remove_variables(self, var_ids):
-        """ Remove variables from the current problem.
-
-        Arguments:
-            var_ids (list): variable identifiers
-        """
-        pass
-
-    def remove_constraint(self, constr_id):
-        """ Remove a constraint from the current problem.
-
-        Arguments:
-            constr_id (str): constraint identifier
-        """
-        pass
-
-    def remove_constraints(self, constr_ids):
-        """ Remove constraints from the current problem.
-
-        Arguments:
-            constr_ids (list): constraint identifiers
-        """
-        pass
-
-    def list_variables(self):
-        """ Get a list of the variable ids defined for the current problem.
-
-        Returns:
-            list: variable ids
-        """
-        return self.var_ids
-
-    def list_constraints(self):
-        """ Get a list of the constraint ids defined for the current problem.
-
-        Returns:
-            list: constraint ids
-        """
-        return self.constr_ids
-
-    def set_bounds(self, bounds_dict):
-        """ Set lower and upper bounds from tuple dictionary
-        Arguments:
-            bounds_dict (dict): lower and upper bounds
-        """
-
-        raise Exception('Not implemented for this solver.')
 
     def update(self):
         """ Update internal structure. Used for efficient lazy updating. """
-        pass
+        
+        if len(self._cached_vars) > 0:
+            self.add_variables(self._cached_vars)
+            self._cached_vars = {}
 
-    def set_objective(self, linear=None, quadratic=None, minimize=True):
-        """ Set a predefined objective for this problem.
+        if len(self._cached_constrs) > 0: 
+            self.add_constraints(self._cached_constrs)
+            self._cached_constrs = {}
 
-        Args:
-            linear (str or dict): linear coefficients (or a single variable to optimize)
-            quadratic (dict): quadratic coefficients (optional)
-            minimize (bool): solve a minimization problem (default: True)
-
-        Notes:
-            Setting the objective is optional. It can also be passed directly when calling **solve**.
-
-        """
-        pass
 
     def build_problem(self, model):
         """ Create problem structure for a given model.
@@ -144,50 +94,73 @@ class Solver(object):
         """
 
         for r_id, reaction in model.reactions.items():
-            self.add_variable(r_id, reaction.lb, reaction.ub, update=False)
-        self.update()
+            self.add_variable(r_id, reaction.lb, reaction.ub)
 
         table = model.metabolite_reaction_lookup()
+
         for m_id in model.metabolites:
-            self.add_constraint(m_id, table[m_id], update=False)
+            self.add_constraint(m_id, table[m_id])
+
         self.update()
 
-    def solve(self, linear=None, quadratic=None, minimize=None, model=None, constraints=None, get_values=True,
-              shadow_prices=False, reduced_costs=False, pool_size=0, pool_gap=None):
+    def solve(self, objective=None, minimize=True, model=None, constraints=None, get_values=True, shadow_prices=False, reduced_costs=False):
         """ Solve the optimization problem.
 
         Arguments:
-            linear (dict): linear objective (optional)
-            quadratic (dict): quadratic objective (optional)
+            objective (dict): linear objective (optional)
             minimize (bool): solve a minimization problem (default: True)
             model (CBModel): model (optional, leave blank to reuse previous model structure)
             constraints (dict): additional constraints (optional)
             get_values (bool or list): set to false for speedup if you only care about the objective value (default: True)
             shadow_prices (bool): return shadow prices if available (default: False)
             reduced_costs (bool): return reduced costs if available (default: False)
-            pool_size (int): calculate solution pool of given size (only for MILP problems)
-            pool_gap (float): maximum relative gap for solutions in pool (optional)
 
         Returns:
             Solution: solution
         """
 
-        # An exception is raised if the subclass does not implement this method.
-        raise Exception('Not implemented for this solver.')
+        if model:
+            self.build_problem(model)
 
-    def get_solution_pool(self, get_values=True):
-        """ Return a solution pool for MILP problems.
-        Must be called after using solve with pool_size argument > 0.
+        self.set_objective(objective, minimize)
 
-        Arguments:
+        if constraints:
+            old_bounds = self.set_bounds(constraints)
 
-            get_values (bool or list): set to false for speedup if you only care about the objective value (default: True)
+        status = self.internal_solve()
 
-        Returns:
-            list: list of Solution objects
+        if status == Status.OPTIMAL:
+            solution = self.get_solution(get_values, shadow_prices, reduced_costs)
+        else:
+            solution = Solution(status)
+
+        if constraints:
+            self.reset_bounds(old_bounds)
+
+        return solution
+
+    def set_objective(self, objective, minimize=False):
+        """ Set a predefined objective for this problem.
+
+        Args:
+            linear (str or dict): linear coefficients (or a single variable to optimize)
+            minimize (bool): solve a minimization problem (default: True)
 
         """
-        raise Exception('Not implemented for this solver.')
+        pass
+
+
+    def set_bounds(self, bounds):
+        pass
+
+    def reset_bounds(self, bounds):
+        pass
+
+    def internal_solve(self):
+        pass
+
+    def get_solution(self, get_values=True, shadow_prices=False, reduced_costs=False):
+        pass
 
     def set_parameter(self, parameter, value):
         """ Set a parameter value for this optimization problem
@@ -199,15 +172,6 @@ class Solver(object):
 
         raise Exception('Not implemented for this solver.')
 
-    def set_parameters(self, parameters):
-        """ Set values for multiple parameters
-
-        Arguments:
-            parameters (dict of Parameter to value): parameter values
-        """
-
-        for parameter, value in parameters.items():
-            self.set_parameter(parameter, value)
 
     def set_logging(self, enabled=False):
         """ Enable or disable log output:
